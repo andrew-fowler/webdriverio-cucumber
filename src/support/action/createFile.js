a445fffef43f7c7ad25fc61b75dd2cb8f3335476
andrew-fowler <4ndrew.fowler@gmail.com>
2017-03-02 13:26:15 +0000

    var to let

--- src/support/action/createFile.js
+++ src/support/action/createFile.js
@@ -2,14 +2,14 @@
  * Created by andrewfowler on 3/1/17.
  */
 module.exports = (name, data, char) => {
-    var datatable = data.raw();
-    var csvContent = "";
+    let fs = require('fs');
+    let datatable = data.raw();
+    let csvContent = "";
 
     datatable.forEach(function(infoArray){
-        var dataString = infoArray.join(char);
+        let dataString = infoArray.join(char);
         csvContent += dataString + "\n";
     });
 
-    var fs = require('fs');
     fs.writeFile(name, csvContent, function(err) {});
 };
