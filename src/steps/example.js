bd3b2266a94a2843dd27beb2e9c800cf3c8521d3
andrew-fowler <4ndrew.fowler@gmail.com>
2017-03-02 13:24:22 +0000

    var to let

--- src/steps/example.js
+++ src/steps/example.js
@@ -6,15 +6,15 @@ import createFile from '../support/action/createFile';
 module.exports = function given() {
     this.Given(/^we have a (csv) with data$/,
         function(ext, data){
-            var filename = "test."+ext;
-            var separator = ",";
+            let filename = "test."+ext;
+            let separator = ",";
             createFile(filename, data, separator);
         } );
 
     this.Given(/^we have a (xls|xlsx|tsv) with data$/,
         function(ext, data){
-            var filename = "test."+ext;
-            var separator = "\t";
+            let filename = "test."+ext;
+            let separator = "\t";
             createFile(filename, data, separator);
         });
 
@@ -27,14 +27,14 @@ module.exports = function given() {
         function(ext){
             browser.chooseFile("input[type='file']", "./test."+ext);
 
-            var btnUpload = browser.element("//input[@type='submit']");
+            let btnUpload = browser.element("//input[@type='submit']");
             btnUpload.click();
         } );
 
     this.Then(/^the filename is displayed$/,
         function(){
-            var panel = browser.element("div[id='uploaded-files']");
-            var successMessage= browser.element("//h3[contains(.,'File Uploaded!')]");
+            let panel = browser.element("div[id='uploaded-files']");
+            let successMessage= browser.element("//h3[contains(.,'File Uploaded!')]");
             expect(panel.getText().toLowerCase()).to.contain('test.');
             expect(successMessage.isVisible()).to.equal(true);
         } );
